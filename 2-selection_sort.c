8c1d48f5f0de7b14515b8e8306292a15c6e4fe6c
ekan07 <aniekanbon@gmail.com>
2022-07-25 16:17:46 +0100

    Add files: 100-shell_sort.c, 101-cocktail_sort_list.c, 102-counting_sort.c, 103-merge_sort.c

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -29,3 +29,37 @@ void selection_sort(int *array, size_t size)
 			print_array(array, size);
 	}
 }
+
+/*
+void selection_sort(int *array, size_t size)
+{
+	int sorted_elnt, swp, tmp;
+	size_t i, j, n = 0, arr_index = 0;
+	
+	if (size < 2)
+		return;
+	for (i = 0; i < size; i++)
+	{
+		for (j = i; j < size - 1; j++)
+		{
+			swp = array[i];
+			if (swp > array[j + 1])
+			{
+				
+				arr_index = j + 1;
+				sorted_elnt = array[arr_index];
+				array[i] = sorted_elnt;
+				array[arr_index] = swp;
+				if (arr_index != size - 1)
+					n = arr_index;
+			}
+		}
+		tmp = array[n];
+		array[n] = array[arr_index];
+		array[arr_index] = tmp;
+		
+		print_array(array, size);
+		
+	}
+}
+*/
